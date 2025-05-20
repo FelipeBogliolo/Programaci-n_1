d1f336fd7d25728edb00132633504b949b191157
FelipeBogliolo <bogliolofelipe@gmail.com>
2025-05-20 21:36:23 +0200

    Práctica parcial 3

---
+++ Práctica_parcial_3.py
@@ -0,0 +1,16 @@
+matriz = [
+    [5, 2, 3, 4],
+    [5, 2, 7, 8],
+    [2, 2, 3, 1],
+    [1, 6, 7, 4]
+]
+
+def tres_en_fila(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            if matriz[i][j] == matriz[i + 1][j] == matriz[i + 2][j]:
+                return matriz[i][j]
+    return None
+            
+resultado = tres_en_fila(matriz)
+print(resultado)
