59d7ef37325c8272c083cebb0fdcbb61215c0dd4
FelipeBogliolo <bogliolofelipe@gmail.com>
2025-05-20 21:57:26 +0200

    Ejercicio 2 algoritmos y ordenamientos

--- Ejercicios_Algoritmos_Ordenamientos/Ejercicio_2.py
+++ Ejercicios_Algoritmos_Ordenamientos/Ejercicio_2.py
@@ -1,7 +1,7 @@
-Nombres = ["Matematica","Investigacion Operativa","Ingles","Literatura","Ciencias Sociales",
+nombres = ["Matematica","Investigacion Operativa","Ingles","Literatura","Ciencias Sociales",
         "Computacion","Ingles","Algebra","Contabilidad","Artistica", "Algoritmos",
         "Base de Datos", "Ergonomia", "Naturaleza"]
-Puntos = [100,98,56,25,87,38,64,42,28,91,66,35,49,57,98]
+puntos = [100,98,56,25,87,38,64,42,28,91,66,35,49,57,98]
 
 def ordenamientos_letras(lista) -> list:
     for i in range(len(lista) - 1):
@@ -11,23 +11,23 @@ def ordenamientos_letras(lista) -> list:
                 lista[i] = lista[j]
                 lista[j] = auxiliar
             elif lista[i] == lista[j]:
-                if Puntos[i] > Puntos[j]:
+                if puntos[i] > puntos[j]:
                     auxiliar = lista[i]
                     lista[i] = lista[j]
                     lista[j] = auxiliar
-                    auxiliar_puntos = Puntos[i]
-                    Puntos[i] = Puntos[j]
-                    Puntos[j] = auxiliar_puntos
+                    auxiliar_puntos = puntos[i]
+                    puntos[i] = puntos[j]
+                    puntos[j] = auxiliar_puntos
                 else:
                     auxiliar = lista[j]
                     lista[j] = lista[i]
                     lista[i] = auxiliar
-                    auxiliar_puntos = Puntos[j]
-                    Puntos[j] = Puntos[i]
-                    Puntos[i] = auxiliar_puntos
+                    auxiliar_puntos = puntos[j]
+                    puntos[j] = puntos[i]
+                    puntos[i] = auxiliar_puntos
     return lista
 
-lista_ordenada = ordenamientos_letras(Nombres)
+lista_ordenada = ordenamientos_letras(nombres)
 print(lista_ordenada)
-for i in Puntos:
+for i in puntos:
     print(i)
