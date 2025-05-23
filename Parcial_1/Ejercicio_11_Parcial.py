19db77e9b69e29d8e47b6cc2fa154afdbb4d81b1
FelipeBogliolo <bogliolofelipe@gmail.com>
2025-05-23 19:54:06 +0200

    Comentarios ejercicio 11 parcial

--- Parcial_1/Ejercicio_11_Parcial.py
+++ Parcial_1/Ejercicio_11_Parcial.py
@@ -1,19 +1,17 @@
-entrada = [10, 20, 30, 40]
-valor = 24
+entrada = [10, 20, 30, 40] #lista de numeros, parametro actual
+valor = 24 #parametro actual
 
-
-
-def calcular_promedio(lista, valor):
-    acum = 0
-    contador = 0
-    for i in range(len(lista)):
-        acum += lista[i]
-        contador += 1
-    promedio = acum / contador
-    if promedio > valor:
-        return True
+def calcular_promedio(lista, valor): #parametros formales lista y valor
+    acum = 0 #acumulador para sumar valores de la lista
+    contador = 0 #contador que cuenta cada valor que contiene la lista
+    for i in range(len(lista)): #recorre la lista completra
+        acum += lista[i] #suma cada valor en el acumulador
+        contador += 1 #suma 1 al contador por cada valor que contiene la lista
+    promedio = acum / contador #calcula un promedio entre valores de ambas variables
+    if promedio > valor: #compara valores y de acuerdo a eso retorna True (en caso de que el promedio es mayor)
+        return True      #False en caso de que sea menor.
     else:
         return False
 
-prueba = calcular_promedio(entrada, valor)
-print(prueba)
+prueba = calcular_promedio(entrada, valor) #la variable prueba invoca la funcion y luego toma el valor del retorno
+print(prueba) #printeamos el valor de la variable prueba, en este caso 'True'
